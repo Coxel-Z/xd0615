ace225631b71a5aed62208bf2afb3d4146867f92
Coxel-Z <zcx287950592@163.com>
2020-06-24 21:12:34 +0800

    第八次作业，张丛笑19050500007

---
+++ lesson8/code.c
@@ -0,0 +1,29 @@
+//Keyboard 音调键盘
+
+  
+void setup()
+{
+  pinMode(8, OUTPUT); //用于给喇叭传输数据
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT); //三个引脚都接收按纽的信号
+}
+
+void loop()
+{
+  
+  if (digitalRead(A0) == HIGH)//如果按下按键1，相连接的A0的线与正极相连，A0就会变成高电平，下两个同理    
+  {
+    tone(8, 440, 100); // 向喇叭传入音调的数据，喇叭发出声音，下同play tone  57 (A4频率= 440 Hz)
+  }
+  if (digitalRead(A1) == HIGH)//如果按下按键2
+  {
+    tone(8, 494, 100); // play tone 59 (B4频率= 494 Hz)
+  }
+  if (digitalRead(A2) == HIGH) //如果按下按键3
+  {
+    tone(8, 523, 100); // play tone 60 (C5频率 = 523 Hz)
+  }
+  //按下这三个按钮，实现A4 B4 C5
+  delay(10); // 适当延迟，使效果真实
+}
